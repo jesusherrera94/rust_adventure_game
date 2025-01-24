b73860b812bb9130f329ecd6e66c1b964bb6a3a7
Jesus Herrera <jesusherrera1594@gmail.com>
2025-01-23 19:50:51 -0600

    read file

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use csv::{ReaderBuilder, StringRecord};
+use std::{fs};
+
+const FILENAME: &str = "history.csv";
+
+fn main() {
+
+    let content = fs::read_to_string(FILENAME).unwrap();
+
+    let mut rdr = ReaderBuilder::new()
+        .delimiter(b';')
+        .from_reader(content.as_bytes());
+
+    for result in rdr.records() {
+        let record = result.unwrap();
+        println!("{:?}", record);
+    }
+
+}
